db69ea33abf4765f854897d4ea1ea6b5a095288c
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2020-02-06 13:17:15 +0100

    Seccion 4.4 - Exercise

--- ExercisesChapter4Class.java
+++ ExercisesChapter4Class.java
@@ -2,13 +2,17 @@ package chapter4;
 
 public class ExercisesChapter4Class {
 
-    public static void main(String[] args) {
-        boolean [] booleanArr = new boolean [3];
+    public static void exercise2Secc44() {
+        int [] first = new int[3];
+        int [] second = {};
+        int [] third = null;
 
-        System.out.println("Elements of the array without explicit initialization: ");
+        System.out.println(first != null ? "first array length: " + first.length : "The array points to null");
+        System.out.println(second != null ? "second array length: " + second.length : "The array points to null");
+        System.out.println(third != null ? "third array length: " + third.length : "The array points to null");
         
-        for (int i = 0; i < booleanArr.length; i++) {
-            System.out.println("Element at position [" + i + "]: " + booleanArr[i]);        
-        }
+    }
+    public static void main(String[] args) {
+        exercise2Secc44();
     }
 }

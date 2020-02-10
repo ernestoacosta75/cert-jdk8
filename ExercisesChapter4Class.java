ad4fcd250cbf6cdeda5a83a817bf212b141406f0
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2020-02-10 13:37:38 +0100

    Seccion 4.4 - First part of Exercise 5

--- ExercisesChapter4Class.java
+++ ExercisesChapter4Class.java
@@ -20,8 +20,36 @@ public class ExercisesChapter4Class {
         }        
     }
 
+    public static void exercise5Secc44() {
+        String[][] names = new String[2][3];
+
+        // Filling the array of String arrays
+        names[0][0] = "Item0ForArrayInIndex0";  
+        names[1][0] = "Item1ForArrayInIndex0"; 
+        System.out.println("Array content in the index 0: ");
+        System.out.println(names[0][0]);
+        System.out.println(names[1][0]);
+        System.out.println();
+
+        names[0][1] = "Item0ForArrayInIndex1";  
+        names[1][1] = "Item1ForArrayInIndex1"; 
+        System.out.println("Array content in the index 1: ");
+        System.out.println(names[0][1]);
+        System.out.println(names[1][1]);
+        System.out.println();        
+
+        names[0][2] = "Item0ForArrayInIndex2";  
+        names[1][2] = "Item1ForArrayInIndex2"; 
+        System.out.println("Array content in the index 2: ");
+        System.out.println(names[0][2]);
+        System.out.println(names[1][2]);
+        System.out.println();        
+    }
+
+
 
     public static void main(String[] args) {
-        exercise3Secc44();
+        // exercise3Secc44();
+        exercise5Secc44();
     }
 }
